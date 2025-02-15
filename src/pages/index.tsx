4991653ead6d1e5d1904c2871d3c244dfd96f729
Mirko Terrazas <mirko.terrazas@zazmic.com>
2025-02-15 18:31:56 -0300

    Base Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,192 +1,157 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
-
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const doclistStyles = {
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  display: `inline-block`,
-  marginBottom: 24,
-  marginRight: 12,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLinks = [
-  {
-    text: "TypeScript Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
-    color: "#8954A8",
-  },
-  {
-    text: "GraphQL Typegen Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/",
-    color: "#8954A8",
-  }
-]
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative" as "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/getting-started/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+import { StaticImage } from 'gatsby-plugin-image';
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={doclistStyles}>
-        {docLinks.map(doc => (
-          <li key={doc.url} style={docLinkStyle}>
-            <a
-              style={linkStyle}
-              href={`${doc.url}?utm_source=starter&utm_medium=ts-docs&utm_campaign=minimal-starter-ts`}
-            >
-              {doc.text}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <ul style={listStyles}>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter-ts`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
+    <div className="font-sans bg-gray-100 h-full flex flex-col">
+      <header className="bg-white text-black p-6">
+        <div className="container mx-auto flex justify-between items-center">
+          <div className="w-full md:w-1/2">
+            <StaticImage
+              width={200}
+              
+              src="../images/hero-logo.png"  // Path relative to this component
+              alt="Hero Image"
+              placeholder="blurred"  // Optional placeholder effect
+              layout="constrained"   // Optional: Choose layout for the image
+            />
+          </div>
+          <nav>
+            <ul className="flex space-x-6">
+              <li><a href="#home-section" className="hover:text-gray-300">Home</a></li>
+              <li><a href="#about-section" className="hover:text-gray-300">About</a></li>
+              <li><a href="#services-section" className="hover:text-gray-300">Services</a></li>
+              <li><a href="#contact-section" className="hover:text-gray-300">Contact</a></li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+
+
+      <div id="home-section" className="relative h-screen flex items-center">
+        {/* Left side: Title and Description */}
+        <div className="w-full md:w-1/2 px-8 text-center md:text-left m-4">
+          <h1 className="text-4xl md:text-5xl font-bold mb-4">Tailored Software Solutions for Your Business</h1>
+          <p className="text-lg mb-6">
+            At TerVarSoft, we provide high-quality, scalable software development services that drive growth. From custom web apps to mobile solutions, our expert team works with you to bring your ideas to life, with a focus on innovation, security, and agility.
+          </p>
+          <a href="#services-section" className="bg-blue-500 text-white py-2 px-6 rounded-lg hover:bg-blue-700 transition duration-200">
+            Explore Our Services
+          </a>
+        </div>
+
+        <StaticImage
+          className="w-full md:w-1/2 h-full bg-cover bg-center"
+          src="../images/hero.jpg"  // Path relative to this component
+          alt="Hero Image"
+          placeholder="blurred"  // Optional placeholder effect
+          layout="constrained"   // Optional: Choose layout for the image
+        />
+        {/* Right side: Background image */}
+   
+      </div>
+
+      <section id="about-section" className="py-16 bg-white">
+        <div className="max-w-7xl mx-auto px-4 text-center">
+          <h2 className="text-3xl font-bold text-gray-800 mb-6">About Us</h2>
+          <p className="text-xl text-gray-600 mb-8">
+            We are a Brazilian company dedicated to bringing the best software development solutions to businesses worldwide. 
+            With a focus on innovation and excellence, we hire the top developers from all across Latin America, ensuring that we provide world-class expertise to our clients.
+          </p>
+          <div className="flex justify-center">
+            <div className="w-1/2 bg-blue-500 h-1 rounded-full"></div>
+          </div>
+        </div>
+      </section>
+
+      <section id="services-section" className="py-16 bg-gray-100">
+        <div className="max-w-7xl mx-auto px-4">
+          <h2 className="text-3xl font-bold text-center text-gray-800 mb-12">Our Services</h2>
+          
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+            {/* Website Creation */}
+            <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+              <StaticImage
+                className="w-full h-48 object-cover" 
+                src="../images/website-creation.jpg"
+                alt="Staff Augmentation"
+                placeholder="blurred" 
+                layout="constrained" 
+              />
+              <div className="p-6">
+                <h3 className="text-2xl font-semibold text-gray-800 mb-4">Websites Creation and Maintenance</h3>
+                <p className="text-gray-600">
+                  We create custom websites and ensure they stay secure and up-to-date, so you can focus on growing your business.
+                </p>
+              </div>
+            </div>
+
+            {/* Staff Augmentation Card */}
+            <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+              <StaticImage
+                className="w-full h-48 object-cover" 
+                src="../images/staff-augmentation.jpg"
+                alt="Staff Augmentation"
+                placeholder="blurred" 
+                layout="constrained" 
+              />
+              <div className="p-6">
+                <h3 className="text-2xl font-semibold text-gray-800 mb-4">Staff Augmentation</h3>
+                <p className="text-gray-600">
+                  Our Staff Augmentation service provides skilled professionals to seamlessly integrate into your team, offering the expertise you need for your projects, without the overhead of recruitment.
+                </p>
+              </div>
+            </div>
+
+            {/* Dedicated Team Card */}
+            <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+              <StaticImage
+                className="w-full h-48 object-cover" 
+                src="../images/dedicated-team.jpg"
+                alt="Staff Augmentation"
+                placeholder="blurred" 
+                layout="constrained" 
+              />
+              <div className="p-6">
+                <h3 className="text-2xl font-semibold text-gray-800 mb-4">Dedicated Team</h3>
+                <p className="text-gray-600">
+                  Our Dedicated Team service assembles a team of experts tailored specifically to your project needs, ensuring long-term collaboration and top-tier delivery for your business.
+                </p>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </section>
+
+      <main id="contact-section" className="py-12 flex-1 bg-white">
+
+        <section className="text-center">
+          <h2 className="text-4xl font-bold text-gray-800 mb-6">We are waiting for you!</h2>
+          <p className="text-lg text-gray-600 mb-8">
+            Are you ready to discuss your next project with us?
+          </p>
+          <a href="mailto:info@tervarsoft.com" className="bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition duration-200">
+            Contact Us
+          </a>
+        </section>
+      </main>
+
+
+      <footer className="bg-gray-800 text-white text-center py-6">
+        <p>&copy; 2025 TerVarSoft. All rights reserved.</p>
+      </footer>
+    </div>
   )
 }
+
+const clients = [
+  {
+    name: "Grafica Tunari",
+    url: "https://graftunari.github.io"
+  }
+];
 
 export default IndexPage
 
